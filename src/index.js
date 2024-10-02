d93920ff7efaadcaef408171f8105a2f63d7d850
dee <178827694+dee797@users.noreply.github.com>
2024-10-02 11:33:37 -0600

    Add tab-switching logic using event listeners

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import { homePageLoad } from "./home.js";
+import { menuPageLoad } from "./menu.js";
 import "./styles.css";
+
+const navBtns = document.querySelectorAll("button");
+
+navBtns[0].addEventListener("click", () => {
+    homePageLoad();
+});
+
+navBtns[1].addEventListener("click", () => {
+    menuPageLoad();
+});
 
 homePageLoad();
